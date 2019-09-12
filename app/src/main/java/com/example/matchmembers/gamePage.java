8f2ac2cb6ad8061bec1912bc24a4286e496df9a5
Lewis Zhang <lzhang19@berkeley.edu>
2019-09-12 01:32:30 -0700

    first commit

---
+++ app/src/main/java/com/example/matchmembers/gamePage.java
@@ -0,0 +1,14 @@
+package com.example.matchmembers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gamePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+    }
+}
