0a3d25238b13275e92fda06e174bce0fc07ba498
Lewis Zhang <lzhang19@berkeley.edu>
2019-09-12 12:32:43 -0700

    Add timer, update components

--- app/src/main/java/com/example/matchmembers/MainActivity.java
+++ app/src/main/java/com/example/matchmembers/MainActivity.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button start;
     TextView title;
+    TextView lastScore;
     public static final int UPDATE_CODE = 1;
 
     @Override
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         start = findViewById(R.id.start_button);
         title = findViewById(R.id.title);
+        lastScore = findViewById(R.id.previousScore);
 
         start.setOnClickListener(this);
     }
@@ -32,6 +36,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent(MainActivity.this, gamePage.class);
                 startActivityForResult(intent, UPDATE_CODE);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == UPDATE_CODE && resultCode == RESULT_OK) {
+            int score = data.getIntExtra("Score", 0);
+            lastScore.setText(String.format("Last Score: %d", score));
         }
     }
 }
--- app/src/main/java/com/example/matchmembers/gamePage.java
+++ app/src/main/java/com/example/matchmembers/gamePage.java
@@ -3,9 +3,13 @@ package com.example.matchmembers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +31,7 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
     Button buttonB;
     Button buttonC;
     Button buttonD;
+    Button endButton;
     TextView timer;
     char correct_button;
 
@@ -38,6 +43,10 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
     HashSet<String> names_to_learn = new HashSet<>();
     int total_names = 0;
 
+    CountDownTimer currentTimer;
+    long milliLeft = 0;
+    static final char TIME_UP_KEY = '0';
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +57,7 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
         buttonB = findViewById(R.id.buttonB);
         buttonC = findViewById(R.id.buttonC);
         buttonD = findViewById(R.id.buttonD);
+        endButton = findViewById(R.id.endGame);
         timer = findViewById(R.id.timer);
         score = findViewById(R.id.score);
         progress = findViewById(R.id.progress);
@@ -56,11 +66,12 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
         getNames();
         Log.d("yeet2", "test");
 
-        setButtons(getRandomButtonOrder(), getRandomNames());
         buttonA.setOnClickListener(this);
         buttonB.setOnClickListener(this);
         buttonC.setOnClickListener(this);
         buttonD.setOnClickListener(this);
+        endButton.setOnClickListener(this);
+        nextQuestion();
     }
 
     @Override
@@ -80,7 +91,42 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
             case R.id.buttonD:
                 processChoice('D');
                 break;
+            case R.id.endGame:
+                endGame();
         }
+    }
+
+    private void endGame() {
+        timerPause();
+
+        // example borrowed from https://stackoverflow.com/questions/2115758/how-do-i-display-an-alert-dialog-on-android
+        new AlertDialog.Builder(gamePage.this)
+                .setTitle("Exit Game")
+                .setMessage("Are you sure you want to exit the game?")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // exit out of game
+                        Intent intent = new Intent(gamePage.this, MainActivity.class);
+                        intent.putExtra("Score", current_score);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        timerResume();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
     }
 
     // 1 = A, 2 = B, 3 = C, 4 = D
@@ -114,22 +160,51 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
 
     private void processChoice(char button_id) {
         // update progress
+        currentTimer.cancel();
+
         current_progress++;
         progress.setText(String.format("Progress: %d%s", (int) (100 * current_progress / total_names), "%"));
 
         if (correct_button == button_id) {
             // update score
             current_score++;
+        } else if (button_id == TIME_UP_KEY) { // if wrong answer, display a Toast
+            Toast.makeText(gamePage.this, "Time Ran Out", Toast.LENGTH_SHORT).show();
         } else { // if wrong answer, display a Toast
             Toast.makeText(gamePage.this, "Wrong Answer", Toast.LENGTH_SHORT).show();
         }
         score.setText(String.format("Score: %d / %d", current_score, current_progress));
 
+        nextQuestion();
+    }
+
+    private void nextQuestion() {
         setButtons(getRandomButtonOrder(), getRandomNames());
-        buttonA.setOnClickListener(this);
-        buttonB.setOnClickListener(this);
-        buttonC.setOnClickListener(this);
-        buttonD.setOnClickListener(this);
+        timerStart(5000); // set a 5 second timer
+    }
+
+    private void timerStart(long timeLeft) {
+        currentTimer = new CountDownTimer(timeLeft, 1000) {
+
+            @Override
+            public void onTick(long milliTillFinish) {
+                milliLeft = milliTillFinish;
+                timer.setText(String.format("%d",milliTillFinish / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                processChoice(TIME_UP_KEY); // any char that's not a,b,c,d (no choice was made, which is by default wrong)
+            }
+        }.start();
+    }
+
+    private void timerPause() {
+        currentTimer.cancel();
+    }
+
+    private void timerResume() {
+        timerStart(milliLeft);
     }
 
     // open names.txt, add all names as Strings to Set object names_to_learn
@@ -151,12 +226,13 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
     }
 
     private String[] getRandomNames() {
-        return new String[]{drawRandomName(), drawRandomName(), drawRandomName(), drawRandomName()};
+        return new String[]{drawRandomName(true), drawRandomName(false),
+                drawRandomName(false), drawRandomName(false)};
     }
 
     // draws random name from names.txt
     // Code borrowed from https://stackoverflow.com/questions/124671/picking-a-random-element-from-a-set
-    private String drawRandomName() {
+    private String drawRandomName(boolean isCorrectAnswer) {
         int size = names_to_learn.size();
         Log.d("size", size+" ");
         int item = new Random().nextInt(size); // In real life, the Random object should be rather more shared than this
@@ -169,7 +245,7 @@ public class gamePage extends AppCompatActivity implements View.OnClickListener 
             }
             i++;
         }
-        names_to_learn.remove(name);
+        if (isCorrectAnswer) names_to_learn.remove(name); // only remove the correct name from the reamining list of names to iterate through
         return name;
     }
 
